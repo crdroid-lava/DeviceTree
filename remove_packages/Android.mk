b9ccc86c31c73d97a2e3835a5da4f758dab1b092
Orkun Ergün <hesaphesapyus@gmail.com>
2022-11-06 12:27:00 +0000

    lava: remove_packages: Include gallery app

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -5,7 +5,7 @@ LOCAL_MODULE := RemovePackages
 LOCAL_SRC_FILES := false
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
-LOCAL_OVERRIDES_PACKAGES := MatLog Jelly ExactCalculator Eleven Etar Recorder DeskClock
+LOCAL_OVERRIDES_PACKAGES := MatLog Jelly ExactCalculator Eleven Etar Gallery2 Recorder DeskClock
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 include $(BUILD_PREBUILT)
